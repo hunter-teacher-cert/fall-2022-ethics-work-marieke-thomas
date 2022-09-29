26b8738fc73a8b8233674f4c615f89bfca7e8a53
Marieke Thomas <32941729+marieke-thomas@users.noreply.github.com>
2022-09-29 00:58:12 +0000

    Edited regex assignment to find names with middle initials

--- week04/regex.py
+++ week04/regex.py
@@ -2,22 +2,21 @@ import re
 
 
 def find_name(line):
-    pattern = r"[A-Z][a-z]+ [A-Z][a-z]+"
-    # pattern = r"([A-Z][a-z]+ ){1,2}[A-Z][a-z]+"
-    # pattern = r'(a(?:bc)?d)'
+    # Two or Three word names (meaning a capital letter followed by lower case letters)
+    pattern = r"([A-Z][a-z]+ [A-Z][a-z]+(?: [A-Z][a-z]+)?)"
     result = re.findall(pattern, line)
 
+    # Names of the format Ms. Thomas
     pattern = r'(?:Mrs\.|Mr\.|Dr\.|Miss|Ms\.) [A-Z][a-z]+'
     result = result + re.findall(pattern, line)
 
-    pattern = r'(Dr\. [A-Z]\. [A-Z][a-z]+|[A-Z]\. [A-Z]\. [A-Z][a-z]+|[A-Z]\. [A-Z][a-z]+)'
+    #Names with single letters such as Dr. S. Howard, J.K. Rowling, etc
+    pattern = r'(Dr\. [A-Z]\. [A-Z][a-z]+|[A-Z]\. ?[A-Z]\. [A-Z][a-z]+|[A-Z][a-z]+ [A-Z]\. [A-Z][a-z]+|[A-Z]\. [A-Z][a-z]+)'
     result = result + re.findall(pattern, line)
-
-    # pattern = r"[A-Z][a-z]+ [A-Z][a-z]+ [A-Z][a-z]+"
-    # result = result + re.findall(pattern,line)
 
     return result
 
+# I chose not to have the program recognize names that are oddly-formatted (such as "marieke thomas") because I don't want it to print pretty much every word it finds.
 
 f = open("names.txt")
 for line in f.readlines():
@@ -25,19 +24,3 @@ for line in f.readlines():
     result = find_name(line)
     if (len(result) > 0):
         print(result)
-
-# import re
-# def find_date(line):
-#     pattern = r"\d{1,2}/\d{1,2}/\d{2,4}"
-#     result = re.findall(pattern,line)
-
-#     pattern=r'(October|Oct|November|Nov)( [0-9]{1,2}, [0-9]{4})'
-#     result = result + re.findall(pattern,line)
-#     return result
-
-# f = open("names.txt")
-# for line in f.readlines():
-#     #print(line)
-#     result = find_date(line)
-#     if (len(result)>0):
-#         print(result)
