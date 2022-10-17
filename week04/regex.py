00db27f9deeeaf838137b779d39019a7b23d745b
Marieke Thomas <32941729+marieke-thomas@users.noreply.github.com>
2022-10-17 23:08:56 +0000

    Added website links

--- week04/regex.py
+++ week04/regex.py
@@ -7,16 +7,17 @@ def find_name(line):
     result = re.findall(pattern, line)
 
     # Names of the format Ms. Thomas
-    pattern = r'(?:Mrs\.|Mr\.|Dr\.|Miss|Ms\.) [A-Z][a-z]+'
+    pattern = r'(?:Mrs\.|M[rs]\.|Dr\.|Miss) [A-Z][a-z]+'
     result = result + re.findall(pattern, line)
 
     #Names with single letters such as Dr. S. Howard, J.K. Rowling, etc
-    pattern = r'(Dr\. [A-Z]\. [A-Z][a-z]+|[A-Z]\. ?[A-Z]\. [A-Z][a-z]+|[A-Z][a-z]+ [A-Z]\. [A-Z][a-z]+|[A-Z]\. [A-Z][a-z]+)'
+    pattern = r'((?:Mrs\.|M[rs]\.|Dr\.|Miss) [A-Z]\. [A-Z][a-z]+|[A-Z]\. ?[A-Z]\. [A-Z][a-z]+|[A-Z][a-z]+ [A-Z]\. [A-Z][a-z]+|[A-Z]\. [A-Z][a-z]+)'
     result = result + re.findall(pattern, line)
 
     return result
 
-# I chose not to have the program recognize names that are oddly-formatted (such as "marieke thomas") because I don't want it to print pretty much every word it finds.
+
+# I chose not to have the program recognize names that are oddly-formatted (such as "marieke thomas") because I don't want it to print pretty much every word it finds. The program also won't recognize a name that's all initials (like MAT or M.A.T.)
 
 f = open("names.txt")
 for line in f.readlines():
@@ -24,3 +25,4 @@ for line in f.readlines():
     result = find_name(line)
     if (len(result) > 0):
         print(result)
+
---
+++ week07/websites.md
@@ -0,0 +1,9 @@
+# Website Links
+## Repo Holding the Website Code
+https://github.com/marieke-thomas/purr-poll-website
+
+## Link to Live Website
+https://marieke-thomas.github.io/purr-poll-website/
+
+## Partner
+Marisa Shuman
